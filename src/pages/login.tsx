be4b886cab6edefc6db1762d1e38c35381919e5d
Hyeonji <kanghyeonji00308@gmail.com>
2025-11-21 20:57:19 +0900

    updated signup & login pages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,43 @@
-import { NavLink } from 'react-router'
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router';
+import axios from 'axios';
 import './login.css'
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('/auth/login', {
+                username,
+                password,
+            });
+
+            const token = response.data.accessToken;
+            localStorage.setItem('accessToken', token);
+
+            alert('로그인 성공');
+            navigate('/profile'); // 프로필 페이지로 이동
+        } catch (err) {
+            console.error(err);
+            alert('아이디 또는 비밀번호가 잘못되었습니다.');
+        }
+    };    
+
     return (
         <div className="login-layout">
         <div className="login-container">
             <NavLink to="/"><div className="login-title">정보대학 이수체계도</div></NavLink>
-            <form action="#">
+            <form onSubmit={handleLogin}>
                 <div>
-                    <input type="text" name="username" placeholder="아이디" required />
+                    <input type="text" placeholder="아이디" required value={username} onChange={(e) => setUsername(e.target.value)} />
                 </div>
                 <div>
-                    <input type="password" name="password" placeholder="비밀번호" required />
+                    <input type="password" placeholder="비밀번호" required value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div>
                     <button type="submit">로그인</button>
@@ -22,4 +48,4 @@ function Login() {
     );
 }
 
-export default Login
+export default Login;
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,17 +1,91 @@
-import { NavLink } from 'react-router'
-import './signup.css'
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router';
+import axios from 'axios';
+import './signup.css';
 
 function Signup() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [nickname, setNickname] = useState('');
+
+    // 아이디, 비번, 닉네임 검증
+    const [usernameError, setUsernameError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [nicknameError, setNicknameError] = useState("");    
+
+    const navigate = useNavigate();
+
+    const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setUsername(e.target.value);
+
+        if (value.length < 5 || value.length > 20) {
+            setUsernameError("아이디는 5자 이상 20자 이하여야 합니다.");
+        } else {
+            setUsernameError("");
+        }
+    };
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setPassword(value);
+
+        const pwRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/;
+
+        if (!pwRegex.test(value)) {
+            setPasswordError("비밀번호는 영문, 숫자를 모두 포함하고 8자 이상 20자 이하여야 합니다.");
+        } else {
+            setPasswordError("");
+        }
+    };
+
+    const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setNickname(value);
+
+        if (value.length < 2 || value.length > 10) {
+            setNicknameError("닉네임은 2자 이상 10자 이하여야 합니다.");
+        } else {
+            setNicknameError("");
+        }
+    };
+
+    const handleSignup = async (e: React.FormEvent) => {
+        e.preventDefault();    
+
+
+        try {
+            await axios.post('/auth/signup', {
+                username,
+                password,
+                nickname
+            });
+
+            alert('회원가입 성공');
+            navigate('/login'); // 로그인 페이지로 이동
+        } catch (err) {
+            console.error(err);
+            alert('회원가입 실패');
+        }
+    };
+
+
     return (
         <div className="signup-layout">
         <div className="signup-container">
             <NavLink to="/"><div className="signup-title">정보대학 이수체계도</div></NavLink>
-            <form action="#">
+            <form onSubmit={handleSignup}>
                 <div>
-                    <input type="text" name="username" placeholder="아이디" required />
+                    {usernameError && <p className="error-text">{usernameError}</p>}
+                    <input type="text" placeholder="아이디" required value={username} onChange={handleUsernameChange} />
                 </div>
                 <div>
-                    <input type="password" name="password" placeholder="비밀번호" required />
+                    {passwordError && <p className="error-text">{passwordError}</p>}
+                    <input type="password" placeholder="비밀번호" required value={password} onChange={handlePasswordChange} />
+                </div>
+                <div>
+                    {nicknameError && <p className="error-text">{nicknameError}</p>}
+                    <input type="text" placeholder="닉네임" required value={nickname} onChange={handleNicknameChange} />
                 </div>
                 <div>
                     <button type="submit">회원가입</button>
@@ -22,4 +96,4 @@ function Signup() {
     );
 }
 
-export default Signup
+export default Signup;
