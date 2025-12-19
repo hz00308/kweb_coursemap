ccaad81a09777a69f17609ebb768324155cdca52
Hyeonji <kanghyeonji00308@gmail.com>
2025-12-19 16:31:50 +0900

    added baseURL to backend

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://curriculum-roadmap-production.up.railway.app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
